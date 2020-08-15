bc2162fc2499e276636b581336cd1cff55bf824f
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2020-08-14 17:54:01 -0700

    wecome >> welcome

--- parts/navbar.php
+++ parts/navbar.php
@@ -11,7 +11,7 @@ echo '<nav class="navbar navbar-expand-lg navbar-light bg-light">
 <div class="collapse navbar-collapse" id="navbarNav">
   <ul class="navbar-nav">
     <li class="nav-item active">
-      <a class="nav-link" href="wecome.php">Welcome<span class="sr-only">(current)</span></a>
+      <a class="nav-link" href="welcome.php">Welcome<span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="signup.php">Signup</a>
