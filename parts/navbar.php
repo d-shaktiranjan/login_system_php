73745fe6ee4c29f24dd6c4083ac65dff193ec24a
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2020-08-14 17:50:36 -0700

    bs: navbar added

---
+++ parts/navbar.php
@@ -0,0 +1,28 @@
+<?php
+
+echo '<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="#">
+    <img src="https://amasty.com/media/catalog/product/cache/2/image/265x/9df78eab33525d08d6e5fb8d27136e95/l/o/login-as-customer-for-magento-1_2x.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+    Login System
+  </a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="wecome.php">Welcome<span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="signup.php">Signup</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="login.php">Login</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="logout.php">Logout</a>
+  </ul>
+</div>
+</nav>';
+
+?>
